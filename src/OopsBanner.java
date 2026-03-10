c63f7cfe5e54d93c4542e5a71f88105d0276b13f
arjunshivdev54-chaos <arjunshivdev54@gmail.com>
2026-03-10 13:34:40 +0530

    UC4 - OOPS banner using String array and loop

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,5 +1,15 @@
-public class OopsBanner {
-    public static void main(String[] args) {
-        System.out.println("  OOPs");
-    }
+public class UC4 {
+    
+}
+public static void main(String[] args) {
+		
+        System.out.println("   ***        ***     ******       *****     ");
+        System.out.println("  **  **     ** **    **    **   **          ");
+        System.out.println(" **    **   **   **   **    **  **           ");
+        System.out.println(" **    **   **   **   **    **   **          ");
+        System.out.println(" **    **   **   **   ******        ****     ");
+        System.out.println(" **    **   **   **   **                **  ");
+        System.out.println(" **    **   **   **   **                 **   ");
+        System.out.println("  **  **     ** **    **                **    ");
+        System.out.println("   ***        ***     **           *****    ");
 }
